6dd65cf7cd32c29a4510de9a3e7505e028a81d57
EsdrasSantosDV <esdrassantosoliveiradeveloper@gmail.com>
2022-09-22 15:22:49 -0300

    INICIALIZANDO PROJETO

---
+++ src/main/java/com/example/springdataestudo/SpringDataEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdataestudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springdataestudo/SpringDataEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdataestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
